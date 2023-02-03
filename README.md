4f4e7c0f4546a3207e5e5a2d65ac4220bd82d30f
Raivis Bruzguls <53197374+siviars@users.noreply.github.com>
2023-02-03 09:14:38 +0200

    Attach application

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Node-web-application
+This is simple application with Node server, which use PUG template engine and Server Side Rendering.
+
+Each menu button render dynamic content from Ajax request.
+<p><img border="0" src="web.jpg"></p>
+
---
+++ public/javascripts/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $("#setMenu1").on('submit', function (event) {
+        event.preventDefault();
+        let value = "Menu1 text, which you can change"
+
+        $.ajax({
+            url: "/menu1",
+            method: "post",
+            contentType: "application/json",
+            data: JSON.stringify({quote: value}),
+            success: function (res) {
+                $("h1").html(`${res.response}`);
+            }
+        });
+    });
+
+    $("#setMenu2").on('submit', function (event) {
+        event.preventDefault();
+        let value = "Menu2 text, which you can change"
+
+        $.ajax({
+            url: "/menu2",
+            method: "post",
+            contentType: "application/json",
+            data: JSON.stringify({quote: value}),
+            success: function (res) {
+                $("h1").html(`${res.response}`);
+            }
+        });
+    });
+
+});
+
